b2830f25546b94e6ab166829b00d7ffd25dab158
CarlaCunibertti <ccunibertti@outlook.com>
2019-06-27 13:21:10 -0500

    Creando html-login-implementadando-css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 /* Manejo del DOM */
+btn=document.getElementById('btn-submit-login');
+btn.addEventListener('click',() => {
+    if (document.form.password.value=='1' && document.form.login.value=='1'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } );
