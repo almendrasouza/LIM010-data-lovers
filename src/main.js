b1e93c47d2a4bb6e06fcb4b6bcc836c93ed10a4f
CarlaCunibertti <ccunibertti@outlook.com>
2019-06-27 18:40:06 -0500

    creando funcion contraseña

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 /* Manejo del DOM */
-btn=document.getElementById('btn-submit-login');
-btn.addEventListener('click',() => {
-    if (document.form.password.value=='1' && document.form.login.value=='1'){ 
-            document.form.submit(); 
-        } 
-        else{ 
-             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-        } 
-    } );
+const loginVista = document.getElementById('vista1');
+const pokemonVista =document.getElementById('pokemon-vista');
+
+
+
+
+const button= document.getElementById('btn-submit-login');
+button.addEventListener('click', () => {
+    const userValue = document.getElementById('name').value;
+    const passwordValue = document.getElementById('Password').value;
+    console.log(userValue);
+    console.log(passwordValue);
+    if (userValue === 'c' && passwordValue === '1') {
+        loginVista.classList.add('hide');
+        pokemonVista.classList.remove('hide');
+    }
+    else {
+        alert("Porfavor ingrese, nombre de usuario y contraseña correctos");
+    
+    }
+});
+
