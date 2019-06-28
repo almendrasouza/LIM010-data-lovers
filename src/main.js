df67aa54c7e259289cf25714a7f384c2c1a57d7b
CarlaCunibertti <ccunibertti@outlook.com>
2019-06-28 13:00:30 -0500

    Cambios en los margenes estilizacion de login

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
 /* Manejo del DOM */
-const loginVista = document.getElementById('vista1');
+const loginVista = document.getElementById('login-box');
 const pokemonVista =document.getElementById('pokemon-vista');
-
-
-
-
-const button= document.getElementById('btn-submit-login');
+const button= document.getElementById('btnlogin');
 button.addEventListener('click', () => {
     const userValue = document.getElementById('name').value;
     const passwordValue = document.getElementById('Password').value;
-    console.log(userValue);
-    console.log(passwordValue);
     if (userValue === 'c' && passwordValue === '1') {
         loginVista.classList.add('hide');
         pokemonVista.classList.remove('hide');
     }
     else {
         alert("Porfavor ingrese, nombre de usuario y contraseña correctos");
-    
-    }
+     }
 });
 
