e59829f5265590f59714aa5936ca5bdc60879432
Almendragit config --global user.email almendrasouzapinedo@gmail.comgit config --listgit config --global user.name <almendrasouzapinedo@gmail.com>
2019-07-17 12:57:45 -0500

    Avanzando test

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-/* Manejo de data */
+/* Manejo de data original*/
 
 // crear funcion para ordenar por nombre con dos parametros, puede ser ciualquier nombre relacionado, y se llamada desde el main.js
 const sortPoke1 = (data, sortOrder) => { // [{},{},{}...] y condicio
@@ -56,8 +56,8 @@ const filterType = (data, type) => {
 const filterWeaknesses = (data, weaknesses) => { //  por debilidad
   let arrayWeaknessesType = [];
   for (let i = 0; i < data.length; i++) {
-    for (let a = 0; a < data[i].weaknesses.length; a++) { //  a ese obj le estoy aplicando su propiead weakn..y obtengo un array de strings y luego recorreidno ese arradestrin
-      if (data[i].weaknesses[a] === weaknesses) { //  siladebiliaddel poke es igual a ladebilidad que escoge el usuario entonces push/o mete el poke dentro de un array
+    for (let ab = 0; ab < data[i].weaknesses.length; ab++) { //  a ese obj le estoy aplicando su propiead weakn..y obtengo un array de strings y luego recorreidno ese arradestrin
+      if (data[i].weaknesses[ab] === weaknesses) { //  siladebiliaddel poke es igual a ladebilidad que escoge el usuario entonces push/o mete el poke dentro de un array
         arrayWeaknessesType.push(data[i]); //  push para ir agregando elementos en el array
       }
     }
@@ -69,7 +69,6 @@ const filterWeaknesses = (data, weaknesses) => { //  por debilidad
 const filterPokByKmEgg = (arrObj, kmEgg) => { //  [{},{}]y el  km de huevo que selecciona el usuario
   let newArrayPokeFilteredByKmEgg = [];
   for (let i = 0; i < arrObj.length; i++) { //  longitud del array de poke o objetos y obtener c/obejto
-    //{}
     if (arrObj[i].egg === kmEgg) { //  da valor string/si cuando al obj le aplicas su propiedad egg.cumple esa condicion
       newArrayPokeFilteredByKmEgg.push(arrObj[i]); //  que coloque este poke dentro del array vacio
     }
