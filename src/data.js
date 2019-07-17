7c12428afdeaabd80abc3a7f2ce4a9a97e969701
Almendragit config --global user.email almendrasouzapinedo@gmail.comgit config --listgit config --global user.name <almendrasouzapinedo@gmail.com>
2019-07-17 12:09:08 -0500

    Actualizando

--- src/data.js
+++ src/data.js
@@ -1,82 +1,95 @@
 /* Manejo de data */
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-/* Manejo de data */
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
 
-const dataInfo = (arrayObjetos) => {//[{17 propiedades},{15 propiedades},{}]un array de pokemones o objetos
-  let newArrayObjPokemonInfo = [];
-  for (let i = 0; i < arrayObjetos.length; i++) {
-    let pokemon = arrayObjetos[i];//{} un pokemon 17 propiedades
-    let pokemonImg = pokemon["img"];
-    // "" string
-    let pokemonName = pokemon["name"];
-    let pokemonHeight = pokemon["height"];// "" string
-    let pokemonId = pokemon["id"];
-    // number
-    let pokemonEgg = pokemon["egg"];
-    // string
-    let pokemonSpawn = pokemon["avg_spawns"];
-    let pokemonWeakness = pokemon["weaknesses"];
-    let pokemonType = pokemon["type"];
-    let pokemonMultipliers= pokemon["multipliers"];
-    //creando un objeto que tengas solo propiedades con la informacion que yo deseo.
-    newArrayObjPokemonInfo.push({
-      img: pokemonImg,//string
-      name: pokemonName,//string
-      height: pokemonHeight,
-      //string
-      id: pokemonId,
-      //number
-      egg: pokemonEgg,//string
-      spawn: pokemonSpawn,//number
-      weakness: pokemonWeakness,//["",""] voy a acceder A un array de string
-      type: pokemonType,// ["",""]
-      multipliers:pokemonMultipliers,
-    });
+// crear funcion para ordenar por nombre con dos parametros, puede ser ciualquier nombre relacionado, y se llamada desde el main.js
+const sortPoke1 = (data, sortOrder) => { // [{},{},{}...] y condicio
+  const arrayNamePoke = data.slice().sort((ab, bc) => { //  slice saca una copia del array de obj, sort lo ordena ese arrydeobj de acuerdo a la condicion que se dice y retorna un nuevoarraydeobjetos ordenado
+    if (ab.name > bc.name) { //  de mayor a menor en letra del abecedario ejm almen y zalet
+      return 1; //  sort esta comparando los nombres de los pokemones y ordenandoles por oden alfabetico
+    }
+    if (ab.name < bc.name) {
+      return -1; //  sort es un high order function(funcion de orden superior)es una funcion q recibe commo parametro otra funcon o retorna otra funcion
+    } //  sort,map y filter son funciones de orden superior
+    return 0; // que se queda en la misma posicion
+  });
+  if (sortOrder === 'A-Z') {
+    return arrayNamePoke;
   }
-  return newArrayObjPokemonInfo;//[{},{}]
-  // [{img:"",name:,img,height,egg,id,...},{},{img:"",name:,img,height,egg,id...}]
-}
-window.dataInfo = dataInfo;
+  if (sortOrder === 'Z-A') {
+    return arrayNamePoke.reverse(); // reverse() para que me devuelva lo contrario , revertir
+  }
 
+  return 0;
+};
 
+const sortSpawn = (data, sortOrder) => {
+  const arraySpawnPoke = data.slice().sort((ab, bc) => {
+    if (ab.avg_spawns > bc.avg_spawns) {
+      return 1;
+    }
+    if (ab.avg_spawns < bc.avg_spawns) {
+      return -1;
+    }
+    return 0;
+  });
+  if (sortOrder === 'Ascendente') {
+    return arraySpawnPoke;
+  }
+  if (sortOrder === 'Descendente') {
+    return arraySpawnPoke.reverse();
+  }
+  return 0;
+};
 
-
-
-
-
-
-
-
-
-//crear funcion por tipo// ME FALTA TEMRINAR //NO TOCAAAAAAAAAAAAAAAAAAAAAAR
-const filterPokemonByType = (arrayObjetos9, condition) => {
-  // [{},{8properties}] aqui entra un array de objetos y una condicion(es un string)
-  let newArrayPokemonFilterbyType = [];
-  for (let i = 0; i < arrayObjetos9.length; i++) {
-    arrayObjetos9[i];//{8 properties}
-    const arrayOfStrings = arrayObjetos9[i].type;
-    //["",""]accediendo a un array de strings
-    for (let i = 0; i < arrayOfStrings.length; i++) {
-      arrayOfStrings[i];
-      let string = arrayOfStrings[i];
-      if (condition === string) {
-        arrayObjetos9[i];//{}
-        newArrayPokemonFilterbyType.push(arrayObjetos9[i]);
+const filterType = (data, type) => {
+  let arrayType = [];
+  for (let i = 0; i < data.length; i++) { //  {},
+    for (let ab = 0; ab < data[i].type.length; ab++) { //  data.type retorna un array de string y recorrer el arr de setring de uno en uno y ahi obtengo el tipo
+      if (data[i].type[ab] === type) { //  filtrar cada poke por su tipo//si el tipodepok es igual al tipo que pone la persona pushea/mete el pokemon dentro de un array
+        arrayType.push(data[i]); //  push para ir agregando elementos en el array
       }
     }
   }
-  return newArrayPokemonFilterbyType;
+  return arrayType; //  [{},{},{}]al final obtengo un arrydeobj donde c/obj representa un pokemon que cumple la condicion
+};
+//  ejemplo si la person  hce clilck en agua, saldra pokemones de  agua un array de  poke de agua
+
+const filterWeaknesses = (data, weaknesses) => { //  por debilidad
+  let arrayWeaknessesType = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let a = 0; a < data[i].weaknesses.length; a++) { //  a ese obj le estoy aplicando su propiead weakn..y obtengo un array de strings y luego recorreidno ese arradestrin
+      if (data[i].weaknesses[a] === weaknesses) { //  siladebiliaddel poke es igual a ladebilidad que escoge el usuario entonces push/o mete el poke dentro de un array
+        arrayWeaknessesType.push(data[i]); //  push para ir agregando elementos en el array
+      }
+    }
+  }
+  return arrayWeaknessesType; // retorna un array de poke que tienen la misma debilidad
 };
 
-window.filterPokemonByType = filterPokemonByType;
+// funcion ara filtrar por tipo de huevo
+const filterPokByKmEgg = (arrObj, kmEgg) => { //  [{},{}]y el  km de huevo que selecciona el usuario
+  let newArrayPokeFilteredByKmEgg = [];
+  for (let i = 0; i < arrObj.length; i++) { //  longitud del array de poke o objetos y obtener c/obejto
+    //{}
+    if (arrObj[i].egg === kmEgg) { //  da valor string/si cuando al obj le aplicas su propiedad egg.cumple esa condicion
+      newArrayPokeFilteredByKmEgg.push(arrObj[i]); //  que coloque este poke dentro del array vacio
+    }
+  }
+  return newArrayPokeFilteredByKmEgg; //  [{egg:2 km},{egg:2km},{egg:2km}]
+};
+
+//  funcion para calcular  el porcentaje de pok por tipo de huevo repecto al total 151
+//  tiene como parametro el result de la funcion FiltePOk.. de la anterior funcion
+const calculatePercentageOfPokeFilteredByKmEgg = (arPokeFilteredByKmEgg) => { //  [{},{},{}]arry de poke ya clasificados por km de huevo
+  const numberOfPoke = arPokeFilteredByKmEgg.length;
+  const percentage = (numberOfPoke * 100) / 151;
+  console.log(percentage);
+  return percentage; //  number
+};
 
 
-
-
-
-
-
-
+window.sortPoke1 = sortPoke1;
+window.sortSpawn = sortSpawn;
+window.filterType = filterType;
+window.filterWeaknesses = filterWeaknesses;
+window.filterPokByKmEgg = filterPokByKmEgg;
+window.calculatePercentageOfPokeFilteredByKmEgg = calculatePercentageOfPokeFilteredByKmEgg;
--- src/main.js
+++ src/main.js
@@ -1,39 +1,168 @@
 /* Manejo del DOM */
-const loginBox = document.getElementById('login-box');
+const loginVista = document.getElementById('login-box');
+const pokemonVista = document.getElementById('pokemon-vista');
 const btnLogin = document.getElementById('btnlogin');
-const pokemonCardDiv = document.getElementById("pokemon-card");
-
+const contenedor = document.getElementById('pokemon-card');
+const pokeData = POKEMON.pokemon;
+const mainHeader = document.getElementById('main-header');
 btnLogin.addEventListener('click', () => {
   const userValue = document.getElementById('username').value;
   const passwordValue = document.getElementById('password').value;
   if (userValue === 'c' && passwordValue === '1') {
-    loginBox.classList.add('hide');
-    pokemonCardDiv.classList.remove('hide');
+    loginVista.classList.add('hide');
+    pokemonVista.classList.remove('hide');
+    mainHeader.classList.remove('hide');
   } else {
-    document.getElementById('result1').innerHTML = "Contraseña errada,intentelo nuevamente";
+    document.getElementById('result1').innerHTML = 'Contraseña errada,intentelo nuevamente';
   }
 });
-/* Manejo del DOM */
 
-const arrayDeObjPokemon = POKEMON.pokemon; //Obtengo array de objetos, c/objeto representa un pokemon
-console.log(arrayDeObjPokemon);
-//obten c/uno de los elementos dentro del array.
-
-dataInfo(arrayDeObjPokemon)
-// [{},{img,name,height,id,egg},{}]
-//console.log(dataInfo(arrayDeObjPokemon));//[{},{img,name,height,id,egg}]
-
-
-const newArrayObjPokemonWith9Keys = dataInfo(arrayDeObjPokemon); //[{},{},{}]
-
-for (let i = 0; i < newArrayObjPokemonWith9Keys.length; i++) {
-newArrayObjPokemonWith9Keys[i]; //{img,name,height,id,egg}
-pokemonCardDiv.innerHTML += `<div class="pokemon-card-style">
-
-<img id="pokemon-img" src="${newArrayObjPokemonWith9Keys[i].img}"alt=""> 
-
-</div>`;
+const showPoke = (pokeData) => {
+  let mostrar = '';
+  for (let i = 0; i < pokeData.length; i++) {
+    if (pokeData[i].multipliers === null) {
+      pokeData[i].multipliers = 0;
+    } else {
+      pokeData[i].multipliers;
+    }
+    let llamado = `
+  
+      <div class= "pokemonvista">
+       <img class="image" src ="${pokeData[i].img}"/>
+         
+         <div class=  "caracteristicas">
+         <p> ${pokeData[i].name}</p>
+         <p>Peso : ${pokeData[i].weight}</p>
+         <p>Estatura : ${pokeData[i].height}</p>  
+         <p>Huevos : ${pokeData[i].egg}</p>
+         <p>Frec. Aparición : ${pokeData[i].avg_spawns}%</p>
+         </div>  
+  
+      </div>`;
+    mostrar += llamado;
+  }
+  return mostrar;
 };
-//hacer un dropdown en html que imprima
+contenedor.innerHTML = showPoke(pokeData);
+// PARA MOSTRAR POKEMONES ORDENADOS A-Z O Z-A
 
+// Creando mi variable selector para almacenar mi opcion de orden
+const selector1 = document.getElementById('order');
+selector1.addEventListener('change', () => {
+  let optionSelector = selector1.value; // alojar en una variable let el valor de mi selector 
+  let pokeOrder; // variable con array vacío para alojar los pokemones ordenados
+  if (optionSelector === 'A-Z') { // condición para que muestre el array que corresponde de acuerdo a la opcion que yo elija
+    pokeOrder = sortPoke1(pokeData, optionSelector);
+  } else if (optionSelector === 'Z-A') {
+    pokeOrder = sortPoke1(pokeData, optionSelector);
+  } else if (optionSelector === 'Ascendente') {
+    pokeOrder = sortSpawn(pokeData, optionSelector);
+  } else if (optionSelector === 'Descendente') {
+    pokeOrder = sortSpawn(pokeData, optionSelector);
+  } else {
+    contenedor.innerHTML = showPoke(pokeData); // muestra pokemones  de mi funcion mostrarPoker de mi data
+  } // console.log(pokeOrder);
+  contenedor.innerHTML = showPoke(pokeOrder); // en el contenedor que me muestre los pokemones de mi data pero ordeandos del array nuevo pokeOrder;
+});
 
+const selector2 = document.getElementById('type-order');
+selector2.addEventListener('change', () => {
+  let optionSelector2 = selector2.value; // alojar en una variable let el valor de mi selector 
+  let pokeFilterType;
+  if (optionSelector2 === 'Bug') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Dark') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Dragon') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Electric') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Fighting') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Fire') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Flying') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Ghost') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Grass') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Ground') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Ice') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Normal') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Poison') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Psychic') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Rock') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else if (optionSelector2 === 'Water') {
+    pokeFilterType = filterType(pokeData, optionSelector2);
+  } else {
+    contenedor.innerHTML = showPoke(pokeData); // muestra pokemones  de mi funcion mostrarPoker de mi data
+  } // console.log(pokeFilterType);
+  contenedor.innerHTML = showPoke(pokeFilterType); // en el contenedor que me muestre los pokemones de mi data pero ordeandos del array nuevo pokeOrder;
+});
+
+const selector3 = document.getElementById('weaknesses-type');
+selector3.addEventListener('change', () => {
+  let optionSelector3 = selector3.value; // alojar en una variable let el valor de mi selector 
+  let pokeFilterWeaknesses;
+  if (optionSelector3 === 'Fire') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Ice') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Flying') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Psychic') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Water') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Ground') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Rock') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Electric') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else if (optionSelector3 === 'Fighting') {
+    pokeFilterWeaknesses = filterWeaknesses(pokeData, optionSelector3);
+  } else {
+    contenedor.innerHTML = showPoke(pokeData); // muestra pokemones  de mi funcion mostrarPoker de mi data
+  } //  console.log(pokeFilterWeaknesses);
+  contenedor.innerHTML = showPoke(pokeFilterWeaknesses); // en el contenedor que me muestre los pokemones de mi data pero ordeandos del array nuevo pokeOrder;
+});
+
+const selector4 = document.getElementById('egg');
+const percentageDiv = document.getElementById('percentage-div');
+selector4.addEventListener('change', () => {
+  let optionSelector4 = selector4.value; // alojar en una variable let el valor de mi selector 
+  let pokeFilterEgg;
+  let percentage;
+  if (optionSelector4 === '2 km') {
+    pokeFilterEgg = filterPokByKmEgg(pokeData, optionSelector4);
+    percentage = calculatePercentageOfPokeFilteredByKmEgg(pokeFilterEgg);
+  } else if (optionSelector4 === '5 km') {
+    pokeFilterEgg = filterPokByKmEgg(pokeData, optionSelector4);
+    percentage = calculatePercentageOfPokeFilteredByKmEgg(pokeFilterEgg);
+  } else if (optionSelector4 === '10 km') {
+    pokeFilterEgg = filterPokByKmEgg(pokeData, optionSelector4);
+    percentage = calculatePercentageOfPokeFilteredByKmEgg(pokeFilterEgg);
+  } else if (optionSelector4 === 'Not in Eggs') {
+    pokeFilterEgg = filterPokByKmEgg(pokeData, optionSelector4);
+    percentage = calculatePercentageOfPokeFilteredByKmEgg(pokeFilterEgg);
+  } else {
+    //  percentage es undefined
+    contenedor.innerHTML = showPoke(pokeData); //  se muestra po defecto
+  }
+  console.log(pokeFilterEgg);
+  if (percentage === undefined) {
+    percentageDiv.innerHTML = '';
+  } else { //  si percentage tiene un valor 
+    percentageDiv.innerHTML = `<p class="percent">El porcentaje de pokemones con ${optionSelector4} es ${percentage.toFixed(2)} % </p>`;;
+  }
+  percentageDiv;
+  contenedor.innerHTML = showPoke(pokeFilterEgg);
+});
